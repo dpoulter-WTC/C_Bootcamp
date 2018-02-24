cac27e196888ded63d161b96bb7279f61764f778
Daniel POULTER <dpoulter@c2r3s4.wethinkcode.co.za>
2018-02-24 17:41:59 +0200

    initial commit

--- code/matchnmatch/ex01/nmatch.c
+++ code/matchnmatch/ex01/nmatch.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   match.c                                            :+:      :+:    :+:   */
+/*   nmatch.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dpoulter <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/02/24 12:11:35 by dpoulter          #+#    #+#             */
-/*   Updated: 2018/02/24 16:04:01 by dpoulter         ###   ########.fr       */
+/*   Created: 2018/02/24 12:50:44 by dpoulter          #+#    #+#             */
+/*   Updated: 2018/02/24 16:20:45 by dpoulter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		match(char *s1, char *s2)
+int		nmatch(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
+	if (!*s1 && !*s2)
 		return (1);
-	else if (*s1 == '\0' || *s2 == '\0')
-		return (0);
-	else if (*s1 == *s2)
-		return (match(s1 + 1, s2 + 1));
-	else if (*s2 == '*')
-		return (match(s1 + 1, s2) || match(s1 + 1, s2 + 1));
+	else if (*s1 == *s2 && *s1 != '*')
+		return (nmatch(s1 + 1, s2 + 1));
+	else if (*s2 == '*' && !*s1)
+		return (nmatch(s1, s2 + 1));
+	else if (*s2 == '*' && *s1)
+		return (nmatch(s1, s2 + 1) + nmatch(s1 + 1, s2));
 	else
 		return (0);
 }
